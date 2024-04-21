05cb2681e20308a67eee615078fd49ec81c8157d
Armaan <indaniarmaan4@gmail.com>
2024-04-21 07:27:45 +0530

    Routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css';
-import Home from './pages/Home';
-import Listing from './pages/Listing';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Listing from "./pages/Listing";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Home />
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/listing" element={<Listing />}></Route>
+        </Routes>
+      </Router>
+    </React.StrictMode>
   );
 }
 
